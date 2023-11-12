e310ede16e98f68b97c756017fa1617cf0876656
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-11-12 20:50:13 +0530

    login

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,44 +1,18 @@
 import React, { useState } from 'react'
 import { Col, Row, Image } from 'antd'
-// import ZerpForm from '../../shared/form/ZerpForm'
 import logo from '../../img/logo.png'
 import { FcGoogle } from 'react-icons/fc'
 import { signInWithGoogle } from '../../firebase/GoogleSignin'
 
 const SignIn = () => {
   const [isLogin, setIsLogin] = useState(true)
-  // const formItems = [
-  //   {
-  //     label: 'Email',
-  //     name: 'email',
-  //     type: 'email',
-  //     placeholder: 'jane@gmail.com',
-  //     noLabel: true
-  //   },
-  //   {
-  //     label: 'Password',
-  //     name: 'password',
-  //     type: 'password',
-  //     placeholder: '******',
-  //     noLabel: true
-  //   }
-  // ]
+
   return (
     <Row>
       <Col
-        xl={10}
-        lg={12}
-        sm={24}
-        md={12}
-        xs={24}
-        className='h-screen flex items-center justify-center'
-      >
-        <Image src={logo} width='30rem' preview={false} />
-      </Col>
-      <Col
-        xl={14}
-        lg={12}
-        md={12}
+        xl={24}
+        lg={24}
+        md={24}
         sm={24}
         xs={24}
         className='flex items-center justify-center bg-red'
@@ -47,11 +21,6 @@ const SignIn = () => {
           <div className='text-[2rem] text-navy font-bold mb-8 text-center'>
             {isLogin ? 'Sign In' : 'Sign up'}
           </div>
-          {/* <ZerpForm
-            formItems={formItems}
-            onFinish={val => console.log(val)}
-            submitText={isLogin ? 'Sign In' : 'Sign up'}
-          /> */}
           <div
             className='rounded-lg h-[2rem] min-w-[5rem] w-full border transition-all hover:border-navy border-sea bg-white px-4 py-1 border-1  cursor-pointer hover:shadow mb-4 flex items-center justify-between hover:bg-navy group'
             onClick={() => signInWithGoogle()}
