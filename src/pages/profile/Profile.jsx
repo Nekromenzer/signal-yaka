32ddece46ca54ef38e553ae233a6a75126e11144
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-11-26 17:32:54 +0530

    Add user profile data and input validation

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Avatar, Typography } from "antd";
 import PageWrapper from "../../layout/PageWrapper";
 
 const Profile = () => {
+  const [userData, setUserData] = useState({
+    email: "bagimshan@gmail.com",
+    telegramId: "user@88",
+    binanceId: "9978797978892",
+    phone: "**********",
+  });
+
+  const dataList = [
+    {
+      id: "email",
+      label: "Email",
+      value: userData.email,
+    },
+    {
+      id: "telegramId",
+      label: "Telegram ID",
+      value: userData.telegramId,
+    },
+    {
+      id: "binanceId",
+      label: "Binance ID",
+      value: userData.binanceId,
+    },
+    {
+      id: "phone",
+      label: "Phone",
+      value: userData.phone,
+    },
+  ];
+
+  const handleValueChange = (text, id) => {
+    if (id === "email") {
+      if (!validateEmail(text)) {
+        return;
+      }
+    }
+    setUserData({
+      ...userData,
+      [id]: text,
+    });
+  };
+
+  const validateEmail = (email) => {
+    // eslint-disable-next-line no-useless-escape
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const { Paragraph } = Typography;
+
   return (
     <PageWrapper>
-      <div>Profile</div>
+      <Card
+        bordered={false}
+        style={{ maxWidth: 600 }}
+        className="drop-shadow-md"
+      >
+        <div className="flex items-center justify-between cursor-pointer">
+          <h1 className="font-semibold">Maneesha Gimshan</h1>
+          <Avatar
+            size={60}
+            className="border-[1px] border-gray-300 hover:shadow-md duration-200 transition-shadow"
+            // profile image url
+            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          />
+        </div>
+        <div className="mt-6">
+          {dataList.map((item, index) => (
+            <div className="flex gap-4 items-center" key={index}>
+              <div className="w-1/5 flex gap-1 justify-between">
+                <Paragraph className="font-semibold">{item.label}</Paragraph>
+                <Paragraph className="font-semibold">:</Paragraph>
+              </div>
+              <Paragraph
+                className="w-full"
+                disabled={item.id === "phone"}
+                editable={
+                  item.id !== "phone" && {
+                    onChange: (text) => {
+                      handleValueChange(text, item.id);
+                    },
+                  }
+                }
+              >
+                {item.value}
+              </Paragraph>
+            </div>
+          ))}
+        </div>
+      </Card>
     </PageWrapper>
   );
 };
