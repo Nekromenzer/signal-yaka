a4b6230bca0a3c5bf63cb731087a4ac5f5854e68
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-11-28 23:14:09 +0530

    refereall fix

--- src/pages/referral/Referral.jsx
+++ src/pages/referral/Referral.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import PageWrapper from "../../layout/PageWrapper";
+import { Typography, Card } from "antd";
 
 const Referral = () => {
+  const { Paragraph } = Typography;
   return (
     <PageWrapper>
-      <div>Referral</div>
+      <Card
+        bordered={false}
+        style={{ maxWidth: 600 }}
+        className="drop-shadow-md"
+      >
+        <div className="font-semibold mb-4">Copy refereral</div>
+        <Paragraph copyable className="text-blue-500">
+          https://ant.design/components/typography
+        </Paragraph>
+      </Card>
     </PageWrapper>
   );
 };
