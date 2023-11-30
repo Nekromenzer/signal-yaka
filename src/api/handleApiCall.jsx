725930372b135dd0eb1b5514c3ba9f0019cad212
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-11-30 20:40:23 +0530

    handle api with headers

--- src/api/handleApiCall.jsx
+++ src/api/handleApiCall.jsx
@@ -1,24 +1,24 @@
-import axios from 'axios'
-import urlDoc from './url'
+import axios from "axios";
+import urlDoc from "./url";
 
 // const baseUrl = import.meta.env.VITE_API_URL
-const baseUrl = 'https://us-central1-infact-zerp.cloudfunctions.net/api/'
+const baseUrl = "https://us-central1-infact-zerp.cloudfunctions.net/api/";
 
 const handleApiCall = ({
-  variant = 'user',
+  variant = "user",
   urlType,
   data,
   params,
-  cb = returnData => returnData,
-  setLoading = state => state,
-  urlParams = '',
-  auth
+  cb = (returnData) => returnData,
+  setLoading = (state) => state,
+  urlParams = "",
+  auth,
 }) => {
-  const url = `${baseUrl}${urlDoc[variant][urlType]?.url}${urlParams}`
-  const method = urlDoc[variant][urlType]?.type
+  const url = `${baseUrl}${urlDoc[variant][urlType]?.url}${urlParams}`;
+  const method = urlDoc[variant][urlType]?.type;
 
-  async function handelCall () {
-    setLoading(true)
+  async function handelCall() {
+    setLoading(true);
     try {
       const response = await axios({
         method,
@@ -26,21 +26,22 @@ const handleApiCall = ({
         data,
         params,
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
           Authorization: auth
-            ? `Bearer ${localStorage.getItem('userToken')}`
-            : ''
-        }
-      })
-      setLoading(false)
-      return cb(response.data, response.status)
+            ? `Bearer ${localStorage.getItem("token")}`
+            : "",
+        },
+      });
+      setLoading(false);
+      return cb(response.data, response.status);
     } catch (error) {
-      setLoading(false)
-      cb(error, error.response?.status)
-      throw error
+      setLoading(false);
+      cb(error, error.response?.status);
+      throw error;
     }
   }
-  return handelCall()
-}
+  return handelCall();
+};
 
-export default handleApiCall
+export default handleApiCall;
