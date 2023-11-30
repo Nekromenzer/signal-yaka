8aacc16a6bc8a7107e84868627a9679ba13497d7
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-11-30 22:32:02 +0530

    marqueee

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Button } from "antd";
 import PageWrapper from "../../layout/PageWrapper";
+import Marquee from "react-fast-marquee";
+import { Alert } from "antd";
 
 const Dashboard = () => {
   return (
     <PageWrapper>
-      <div className="bg-pink-400 hover:bg-blue-500">
-        <Button type="primary">Button</Button>
+      <div className="2xl:mx-24">
+        <Alert
+          banner
+          message={
+            <Marquee
+              pauseOnHover
+              gradient={false}
+              className="text-[1rem] font-semibold"
+            >
+              Please enter your Required data in profile
+            </Marquee>
+          }
+        />
+        <Alert
+          className="mt-8"
+          banner
+          message={
+            <Marquee
+              pauseOnHover
+              gradient={false}
+              className="text-[1rem] font-semibold"
+            >
+              Please make payment using link or qr code then contact Admin for
+              the account activation
+            </Marquee>
+          }
+        />
       </div>
     </PageWrapper>
   );
