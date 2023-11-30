4a680c8e38ca231007522ac03734545cdfef1038
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-11-30 20:40:05 +0530

    save uid

--- src/firebase/GoogleSignin.js
+++ src/firebase/GoogleSignin.js
@@ -9,12 +9,14 @@ export const signInWithGoogle = () => {
       const name = result.user.displayName;
       const email = result.user.email;
       const profilePic = result.user.photoURL;
+      const uid = result.user.uid;
       const token = await result.user.getIdToken();
-
+      console.log(result);
       localStorage.setItem("name", name);
       localStorage.setItem("email", email);
       localStorage.setItem("profilePic", profilePic);
       localStorage.setItem("token", token);
+      localStorage.setItem("uid", uid);
       return (window.location.href = "/");
     })
     .catch((error) => {
