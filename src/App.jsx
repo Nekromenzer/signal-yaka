c2310cd89960ae2b867ad359e80d413802b70ff4
Maneesha Gimshan <bamaneeshagimshan1999@gmail.com>
2023-12-02 13:31:23 +0530

    dashboard refresh bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
-import Dashboard from "./pages/Dashboard/Dashboard";
 import SignIn from "./pages/signIn/SignIn";
 import HandleAuth from "./auth/HandleAuth";
 import Referral from "./pages/referral/Referral";
@@ -9,7 +8,7 @@ import Profile from "./pages/profile/Profile";
 import Ref from "./pages/refCheck/ref";
 import CreateProfile from "./pages/createProfile/createProfile";
 import Home from "./pages/home/Home";
-// import Home from './pages/Home/Home'
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
@@ -18,7 +17,7 @@ function App() {
       <Route path="ref/:id" element={<Ref />} />
       <Route path="login" element={<SignIn />} />
       <Route element={<HandleAuth />}>
-        <Route index path="/" element={<CreateProfile />} />
+        {/* <Route index path="/" element={<CreateProfile />} /> */}
         <Route element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="referral" element={<Referral />} />
